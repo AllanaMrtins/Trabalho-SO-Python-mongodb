f1a2f82a2498cc95b9eefc2a5c0e227a769c3c90
Janiele-B <ryzenjaniele2025@gmail.com>
2026-06-26 23:45:09 -0300

    Add POST route for creating atendimentos

--- routes/atendimentos.py
+++ routes/atendimentos.py
@@ -1,12 +1,10 @@
 from flask import Blueprint
-from controllers.atendimento_controller import listar_atendimentos
+from controllers.atendimento_controller import (
+    listar_atendimentos,
+    criar_atendimento
+)
 
-atendimento_bp = Blueprint(
-    "atendimentos",
-    __name__
-    )
+atendimento_bp = Blueprint("atendimentos", __name__)
 
-atendimento_bp.route(
-    "/atendimentos",
-    methods=["GET"]
-    )(listar_atendimentos)
+atendimento_bp.route("/atendimentos", methods=["GET"])(listar_atendimentos)
+atendimento_bp.route("/atendimentos", methods=["POST"])(criar_atendimento)
