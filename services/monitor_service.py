270fd927d2d8203e766a27489a952919fbb1d547
Janiele-B <ryzenjaniele2025@gmail.com>
2026-06-26 23:02:55 -0300

    Implement MonitorService with listing and search methods

---
+++ services/monitor_service.py
@@ -0,0 +1,13 @@
+from repositories.monitor_repository import MonitorRepository
+
+class MonitorService:
+
+    @staticmethod
+    def listar():
+        return MonitorRepository.listar()
+
+    @staticmethod
+    def buscar_por_matricula(matricula):
+        return MonitorRepository.buscar_por_matricula(
+            matricula
+        )
