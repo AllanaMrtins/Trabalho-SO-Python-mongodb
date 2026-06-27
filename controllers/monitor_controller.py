aa93e0d82b19df64143907f4b314fd0d1c1dee17
Janiele-B <ryzenjaniele2025@gmail.com>
2026-06-26 23:42:11 -0300

    Add create monitor function to monitor_controller
    Added a new function to create a monitor and updated imports.

--- controllers/monitor_controller.py
+++ controllers/monitor_controller.py
@@ -1,7 +1,21 @@
-from flask import jsonify
+from flask import jsonify, request
+from models.monitor import Monitor
 from services.monitor_service import MonitorService
 
+
 def listar_monitores():
-    return jsonify(
-        MonitorService.listar()
+    return jsonify(MonitorService.listar())
+
+
+def criar_monitor():
+    data = request.get_json()
+
+    monitor = Monitor(
+        matricula=data["matricula"],
+        nome=data["nome"],
+        email=data["email"],
+        disciplina=data["disciplina"],
+        horario=data["horario"]
     )
+
+    return jsonify(MonitorService.criar(monitor))
