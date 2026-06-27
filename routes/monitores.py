db1c32cbd31ced0edf927cd89a79112051177bbd
Janiele-B <ryzenjaniele2025@gmail.com>
2026-06-26 23:41:29 -0300

    Add POST route for creating monitors

--- routes/monitores.py
+++ routes/monitores.py
@@ -1,12 +1,7 @@
 from flask import Blueprint
-from controllers.monitor_controller import listar_monitores
+from controllers.monitor_controller import listar_monitores, criar_monitor
 
-monitor_bp = Blueprint(
-    "monitores",
-    __name__
-    )
+monitor_bp = Blueprint("monitores", __name__)
 
-monitor_bp.route(
-    "/monitores",
-    methods=["GET"]
-    )(listar_monitores)
+monitor_bp.route("/monitores", methods=["GET"])(listar_monitores)
+monitor_bp.route("/monitores", methods=["POST"])(criar_monitor)
