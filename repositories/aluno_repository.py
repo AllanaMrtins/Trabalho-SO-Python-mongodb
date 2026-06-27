139dcc99ddaf105a5fc608d4c16916d5efbe4851
Janiele-B <ryzenjaniele2025@gmail.com>
2026-06-26 23:05:35 -0300

    Add AlunoRepository for student database operations

---
+++ repositories/aluno_repository.py
@@ -0,0 +1,41 @@
+from database.connection import MongoConnection
+
+class AlunoRepository:
+
+    collection = MongoConnection.get_database()["alunos"]
+
+    @staticmethod
+    def inserir(aluno):
+        return AlunoRepository.collection.insert_one(
+            aluno.to_dict()
+        )
+
+    @staticmethod
+    def listar():
+        return list(
+            AlunoRepository.collection.find(
+                {},
+                {"_id": 0}
+            )
+        )
+
+    @staticmethod
+    def buscar_por_matricula(matricula):
+        return AlunoRepository.collection.find_one(
+            {"matricula": matricula},
+            {"_id": 0}
+        )
+    
+    @staticmethod
+    def aluno_esta_matriculado_na_disciplina(
+        matricula_aluno,
+        disciplina
+    ):
+        aluno = AlunoRepository.collection.find_one(
+            {
+                "matricula": matricula_aluno,
+                "disciplinas": disciplina
+            }
+        )
+
+        return aluno is not None
