b3aeaee5e4818f99068805229e54282e1efbe1b8
Janiele-B <ryzenjaniele2025@gmail.com>
2026-06-26 23:03:22 -0300

    Add AlunoService with listing and search methods

---
+++ services/aluno_service.py
@@ -0,0 +1,13 @@
+from repositories.aluno_repository import AlunoRepository
+
+class AlunoService:
+
+    @staticmethod
+    def listar():
+        return AlunoRepository.listar()
+
+    @staticmethod
+    def buscar_por_matricula(matricula):
+        return AlunoRepository.buscar_por_matricula(
+            matricula
+        )
