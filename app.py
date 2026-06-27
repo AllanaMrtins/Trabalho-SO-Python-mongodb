5d47292716ddbf028ae17e54e99aae256f91bb80
Janiele-B <ryzenjaniele2025@gmail.com>
2026-06-26 22:46:41 -0300

    Add route blueprints and update JSON responses
    Register blueprints for aluno, monitor, atendimento, and replica routes. Update JSON response formatting for home and health routes.

--- app.py
+++ app.py
@@ -1,25 +1,38 @@
 from flask import Flask, jsonify
 
+from routes.aluno_routes import aluno_bp
+from routes.monitor_routes import monitor_bp
+from routes.atendimento_routes import atendimento_bp
+from routes.replica_routes import replica_bp
+
 app = Flask(__name__)
+
+app.register_blueprint(aluno_bp)
+app.register_blueprint(monitor_bp)
+app.register_blueprint(atendimento_bp)
+app.register_blueprint(replica_bp)
+
 
 @app.route("/")
 def home():
     return jsonify(
         {
-            "Sistema" : "SIGMON",
-            "Descricao": "Sistema Integrado de gestão de monitorias",
+            "Sistema": "SIGMON",
+            "Descricao": "Sistema Integrado de Gestão de Monitorias",
             "Universidade": "UFPI",
             "Status": "online"
         }
     )
 
+
 @app.route("/health")
 def health():
     return jsonify(
         {
-            "status" : "ok"
+            "status": "ok"
         }
     )
+
 
 if __name__ == "__main__":
     app.run(
